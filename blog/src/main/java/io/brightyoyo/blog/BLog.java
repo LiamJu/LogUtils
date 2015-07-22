958f5ce0ca73fcc7c2d46bb6e24c14663d0f9627
goodev <goodev@promote.cache-dns.local>
2015-07-22 22:19:17 +0800

    add Blog file

---
+++ blog/src/main/java/io/brightyoyo/blog/BLog.java
@@ -0,0 +1,14 @@
+package io.brightyoyo.blog;
+
+import android.util.Log;
+
+/**
+ * @author goodev
+ * @since 15/7/22
+ */
+public class BLog {
+
+    public static void logI(String tag, String msg) {
+        Log.i(tag, msg);
+    }
+}
